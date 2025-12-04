bdd897ca66f1a1f626bcbd38b41cace71d5b70bd
Nathan Nguyen <nathnguyen7631@gmail.com>
2025-12-04 03:29:48 -0800

    implemented listInternetService

---
+++ base_model_commands.py
@@ -0,0 +1,14 @@
+def listInternetService(cursor, id):
+    cursor.execute(f'''SELECT s.sid, s.endpoints, s.provider
+                   FROM InternetService AS s JOIN ModelServices AS ms ON s.sid = ms.sid
+                   WHERE ms.bmid = {id}
+                   ORDER BY s.provider ASC''')
+    
+    response = cursor.fetchall()
+    for i in range(len(response)):
+        res = response[i]
+        response[i] = f'{res[0]},{res[1]},{res[2]}'
+    return response    
+
+def countCustomizedModel(cursor, ids):
+    pass
